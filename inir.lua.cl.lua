a4405078e273aecca442c2d1d26d08e128011e61
C0nfuse C0de <86929555+c0nfuse-code@users.noreply.github.com>
2021-07-04 23:33:02 +0400

    init

---
+++ inir.lua.cl.lua
@@ -0,0 +1,18 @@
+-- init**
+-- С0здан0 К0нФуЗ-Код 2021 (с)0nfuse(Code) - (Создано мною) 
+
+zashita = nul
+
+netwr if netwok start == exploir exploit** вернее do
+
+while zashita = true 
+do crash-- end
+
+crash=igrok();
+
+string n=null;
+
+net.da("expl0it", function ()ban end)
+
+
+end
